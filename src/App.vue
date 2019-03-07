4dee85b93964112ef90997343c66e1d627737bfc
xofserve <samgain69@gmail.com>
2019-03-07 17:56:23 -0500

    Vue for Router

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
     <router-view/>
   </div>
 </template>
-
+ 
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 
 Vue.use(Router)
-
+ 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
